bdc86d1d05f89cfd7e43c6371209da0f042dce90
cdemih00 <cdemih00@estudiantes.unileon.es>
2025-03-06 16:58:39 +0100

    ejercicio6

--- practica1/problema6.c
+++ practica1/problema6.c
@@ -13,21 +13,33 @@ int main() {
     printf("Variable / funcion Direccion                  Tamaño\n");
     printf("              main %p\n", main);
     printf("          variable %p  %12ld\n", &variable, sizeof(variable));
-    int main ( void ) {
         int pid = fork () ;
         if(pid==0){
-            printf (" Soy el hijo y mi pid es: %5d\n",getpid());
+            printf ("Soy el hijo y mi pid es:%d\n",getpid());
+            int variable_hijo = 10;
             char cmd[50];
             int variable =5;
             sprintf(cmd, "cat /proc/%d/maps", getpid());
-            printf("Mapa de memoria antes de crear el hilo:\n");
+            printf("Mapa de memoria en el hijo:\n");
             system(cmd);
             printf("--\n");
             printf("Variable / funcion Direccion                  Tamaño\n");
             printf("              main %p\n", main);
-            printf("          variable %p  %12ld\n", &variable, sizeof(variable));
+            printf("          variable %p  %12ld\n", &variable_hijo, sizeof(variable_hijo));
         }
         else{
-        printf (" Soy el padre , mi pid es: %5d y el pid de mi hijo es: %5d\n", getpid () , pid ) ;
+            sleep(3);
+            printf (" Soy el padre , mi pid es:%d y el pid de mi hijo es:%d\n", getpid () , pid ) ;
+            int variable_padre = 15;
+            char cmd[50];
+            int variable =5;
+            sprintf(cmd, "cat /proc/%d/maps", getpid());
+            printf("Mapa de memoria en el hijo:\n");
+            system(cmd);
+            printf("--\n");
+            printf("Variable / funcion Direccion                  Tamaño\n");
+            printf("              main %p\n", main);
+            printf("    variable_padre %p  %12ld\n", &variable_padre, sizeof(variable_padre));
+            sleep(3);
         }
 }
